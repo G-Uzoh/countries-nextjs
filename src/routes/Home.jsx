29497476283e051f2d6cda962175dbf5d60178fe
George Uzoh <ugochukwuuzoh@gmail.com>
2024-03-18 23:30:12 +0200

    add text to home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: 20}}>
+      <h1>Countries App</h1>
+      <p>This web app lets the user view information about all the countries of the world.</p>
+      <p>To get started, please register with valid credentials. If you already have a user account, simply login to view countries.</p>
     </div>
   );
 };
